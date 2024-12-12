2c6204dfb835b6843867f57a3782a98391ff8f79
= <febarchapray@gmail.com>
2024-12-13 01:02:33 +0530

    created a predicting page for individual user

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require("path")
 const {connectToDB} = require('./db')
 const Health = require('./models/dataset')
 const monitorDocumentUntilOutcomeUpdated = require('./monitoring')
+const cors = require('cors');
 
 let app = express();
 let PORT = 8000;
@@ -14,9 +15,13 @@ connectToDB('mongodb://127.0.0.1:27017/diabection').then(()=>{
 
 
 // Middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
 app.use(express.urlencoded({extended:false}))
 app.set('view engine', 'ejs')
 app.set('views', path.resolve('./views'))
+app.use(cors());  // This allows all origins; adjust as necessary for security
+
 
 
 // Routes
@@ -33,18 +38,37 @@ app.post('/', async(req, res)=>{
     // console.log(` Username: ${userData.username}\n Pregnancy: ${userData.pregnancies}\n Glucose: ${userData.glucose}, Blood Pressure: ${userData.bp}\n Skin Thickness: ${userData.skinthickness}\n Insulin: ${userData.insulin}\n BMI: ${userData.bmi}\n DiabetesPedigreeFunction: ${userData.dpf}\n Age: ${userData.age}`)
 
 
-    return res.render('result', {data})
+    // return res.render('result', {data})
+    return res.redirect('/result')
 })
 
 app.get('/result', async(req, res)=>{
     let data = await Health.find({})
-    console.log(data)
     return res.render('result', {data})
 })
 
 app.get('/prediction', async(req, res)=>{
-    res.render('prediction')
+    return res.render('prediction')
 })
+
+// app.post('/prediction', async(req, res)=>{
+//     let data = req.body
+
+//     console.log(data)
+//     return res.render('prediction')
+// })
+
+
+
+app.post('/prediction', async (req, res) => {
+    // Handle the request here
+    let username = req.body.username
+
+    let userData = await Health.findOne({username})
+    console.log(userData)
+
+    return res.render('prediction', {username});
+});
 
 
 
